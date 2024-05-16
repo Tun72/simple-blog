c728ee6dfb2f255cc3917b443a80aa6d232b7cd3
Tun72 <tunt72553@gmail.com>
2024-05-16 13:29:31 +0630

    master

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,49 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import AppLayout from "./ui/AppLayout";
+import Error from "./ui/Error";
+import Home from "./pages/Home";
+import Posts from "./pages/Posts";
+import Create from "./pages/Create";
+import Auth from "./pages/Auth";
+import Details from "./pages/Details";
+import Edit from "./pages/Edit";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Posts />,
+      },
+      {
+        path: "/post/create",
+        element: <Create />,
+      },
+      {
+        path: "/auth",
+        element: <Auth />,
+      },
+      {
+        path: ":id",
+        id: "post-detail",
+        children: [
+          {
+            index: true,
+            element: <Details />,
+          },
+          {
+            path: "/post/edit",
+            element: <Edit />,
+          },
+        ],
+      },
+    ],
+  },
+]);
 function App() {
-  return (
-    <div>
-      
-    </div>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
